03ba9e33a896f61150d9d7dcaa99fc9d5c22ff60
bereftHeart <nguyenhuudat171203@gmail.com>
2025-02-23 14:49:10 +0700

    fix: CORS

--- backend/src/utils/response.ts
+++ backend/src/utils/response.ts
@@ -5,7 +5,7 @@ export const response = (statusCode: number, message: string | object) => ({
   statusCode,
   headers: {
     "Access-Control-Allow-Origin": "*", // Allow all origins
-    "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+    "Access-Control-Allow-Methods": "OPTIONS, GET, POST, PUT, DELETE",
     "Access-Control-Allow-Headers": "Content-Type, Authorization",
   },
   body: JSON.stringify(typeof message === "string" ? { message } : message),
